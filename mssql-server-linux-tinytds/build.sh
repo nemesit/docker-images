6597e9b34008f1caeafacc881933d1998a7d6c47
Felix Grabowski <fukurokujoe@googlemail.com>
2020-02-20 23:31:38 +0100

    2019 tinytds docker

--- mssql-server-linux-tinytds/build.sh
+++ mssql-server-linux-tinytds/build.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-tag=2017-GA
+tag=2019-latest
 
 images=$(docker images --all --quiet)
 if [[ -n $images ]]; then
   docker rmi --force $images
 fi
 
-docker pull microsoft/mssql-server-linux:2017-GA
-docker build --no-cache -t metaskills/mssql-server-linux-tinytds .
+docker pull mcr.microsoft.com/mssql/server:2019-latest
+docker build --no-cache -t nemesit/mssql-server-linux-tinytds .
 
-docker run -p 1433:1433 -d metaskills/mssql-server-linux-tinytds
+docker run -p 1433:1433 -d nemesit/mssql-server-linux-tinytds
 sleep 15
-container=$(docker ps --all --quiet --filter ancestor=metaskills/mssql-server-linux-tinytds)
+container=$(docker ps --all --quiet --filter ancestor=nemesit/mssql-server-linux-tinytds)
 
 docker exec $container /opt/tinytds/setup.sh
-docker commit $container metaskills/mssql-server-linux-tinytds
-docker tag metaskills/mssql-server-linux-tinytds metaskills/mssql-server-linux-tinytds:$tag
-docker push metaskills/mssql-server-linux-tinytds:$tag
-docker push metaskills/mssql-server-linux-tinytds
+docker commit $container nemesit/mssql-server-linux-tinytds
+docker tag nemesit/mssql-server-linux-tinytds nemesit/mssql-server-linux-tinytds:$tag
+docker push nemesit/mssql-server-linux-tinytds:$tag
+docker push nemesit/mssql-server-linux-tinytds
 docker stop $container
