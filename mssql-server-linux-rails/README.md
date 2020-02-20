48b86d48e39dadc201ee8197288d1304588d303c
Felix Grabowski <fukurokujoe@googlemail.com>
2020-02-20 19:34:34 +0100

    update to latest ms sqlserver

--- mssql-server-linux-rails/README.md
+++ mssql-server-linux-rails/README.md
@@ -26,19 +26,24 @@ Fully automated build
 Simple build command.
 
 ```shell
-docker build -t metaskills/mssql-server-linux-rails .
+docker build -t nemesit/mssql-server-linux-rails .
 ```
 
 Ensure that the build script worked.
 
 ```shell
-docker run -p 1433:1433 -d metaskills/mssql-server-linux-rails
-container=$(docker ps --all --quiet --filter ancestor=metaskills/mssql-server-linux-rails)
+docker run -p 1433:1433 -d nemesit/mssql-server-linux-rails
+container=$(docker ps --all --quiet --filter ancestor=nemesit/mssql-server-linux-rails)
 docker exec -it $container bash
 
 /opt/mssql-tools/bin/sqlcmd -U sa -P super01S3cUr3 -S localhost -Q "SELECT name FROM master.dbo.sysdatabases"
 /opt/mssql-tools/bin/sqlcmd -U sa -P super01S3cUr3 -S localhost -Q "SELECT loginname, dbname FROM syslogins"
-tsql -H localhost -p 1433 -U rails -P '' -D activerecord_unittest
+
+# if needed
+# yarn install sql-cli
+
+mssql -u rails -p '' -s  localhost -o 1433 -d activerecord_unittest
+.quit
 
 docker stop $container
 ```
--- mssql-server-linux-rails/build.sh
+++ mssql-server-linux-rails/build.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-tag=2017-GA
+tag=2019-latest
 
 images=$(docker images --all --quiet)
 if [[ -n $images ]]; then
   docker rmi --force $images
 fi
 
-docker pull microsoft/mssql-server-linux:2017-GA
-docker build --no-cache -t metaskills/mssql-server-linux-rails .
+docker pull mcr.microsoft.com/mssql/server:2019-latest
+docker build --no-cache -t nemesit/mssql-server-linux-rails .
 
-docker run -p 1433:1433 -d metaskills/mssql-server-linux-rails
+docker run -p 1433:1433 -d nemesit/mssql-server-linux-rails
 sleep 15
-container=$(docker ps --all --quiet --filter ancestor=metaskills/mssql-server-linux-rails)
+container=$(docker ps --all --quiet --filter ancestor=nemesit/mssql-server-linux-rails)
 
 docker exec $container /opt/rails/setup.sh
-docker commit $container metaskills/mssql-server-linux-rails
-docker tag metaskills/mssql-server-linux-rails metaskills/mssql-server-linux-rails:$tag
-docker push metaskills/mssql-server-linux-rails:$tag
-docker push metaskills/mssql-server-linux-rails
+docker commit $container nemesit/mssql-server-linux-rails
+docker tag nemesit/mssql-server-linux-rails nemesit/mssql-server-linux-rails:$tag
+docker push nemesit/mssql-server-linux-rails:$tag
+docker push nemesit/mssql-server-linux-rails
 docker stop $container
